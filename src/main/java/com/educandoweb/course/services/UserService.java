11f7abe587aeed78dbd472f4297abadf33026e54
ricardojdsilva <ricardojd@gmail.com>
2021-02-17 18:35:02 -0800

    exeption delete

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -38,29 +38,28 @@ public class UserService {
 	
 	public void delete(Long id) {
 		
-		
-		repository.deleteById(id);
-		/*try {
+		//repository.deleteById(id);
+		try {
 			repository.deleteById(id);
 		} catch (EmptyResultDataAccessException e) {
 			throw new ResourceNotFoundException(id);
 		} catch (DataIntegrityViolationException e) {
 			throw new DatabaseException(e.getMessage());
-		}*/
+		}
 	}
 	
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getOne(id);
-		updateData(entity, obj);
-		return repository.save(entity);
-		/*try {
+		//User entity = repository.getOne(id);
+		//updateData(entity, obj);
+		//return repository.save(entity);
+		try {
 			User entity = repository.getOne(id);
 			updateData(entity, obj);
 			return repository.save(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException(id);
-		}	*/
+		}	
 	}
 
 	private void updateData(User entity, User obj) {

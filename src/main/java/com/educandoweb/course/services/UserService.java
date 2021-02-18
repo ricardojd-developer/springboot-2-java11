30526f48212746123dd8ce8dfb5f01b7fce80d6e
ricardojdsilva <ricardojd@gmail.com>
2021-02-17 16:42:10 -0800

    update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -49,20 +49,23 @@ public class UserService {
 		}*/
 	}
 	
-	/*
+	
 	public User update(Long id, User obj) {
-		try {
+		User entity = repository.getOne(id);
+		updateData(entity, obj);
+		return repository.save(entity);
+		/*try {
 			User entity = repository.getOne(id);
 			updateData(entity, obj);
 			return repository.save(entity);
 		} catch (EntityNotFoundException e) {
 			throw new ResourceNotFoundException(id);
-		}	
+		}	*/
 	}
 
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-	}*/
+	}
 }
